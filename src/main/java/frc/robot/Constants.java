59fb8da756d7d19814ca00d3dd7168b4480700ac
6904terawatts <46202952+6904terawatts@users.noreply.github.com>
2024-02-27 21:51:59 -0800

    Add files via upload

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,11 +15,59 @@ package frc.robot;
 public final class Constants {
   
 
-public static final int rightFrontMotorId = 21;
-public static final int leftFrontMotorId = 22;
-public static final int rightBackMotorId = 23;
-public static final int leftBackMotorId = 24;
+public static final int rightFrontMotorId = 22;
+public static final int leftFrontMotorId = 23;
+public static final int rightBackMotorId = 24;
+public static final int leftBackMotorId = 21;
 public static final int leftJoystickId = 1;
 public static final int rightJoystickId = 2;
 public static final double kArcadeDeadBand = .2;
+
+
+
+
+
+public static class OperatorConstants {
+    // Port numbers for driver and operator gamepads. These correspond with the numbers on the USB
+    // tab of the DriverStation
+    public static final int kOperatorControllerPort = 0
+    ;
+
+
+  }
+
+
+public static class LauncherConstants {
+    // PWM ports/CAN IDs for motor controllers
+    public static final int kFeederID = 1;
+    public static final int kLauncherID = 2;
+
+    // Current limit for launcher and feed wheels
+    public static final int kLauncherCurrentLimit = 80;
+    public static final int kFeedCurrentLimit = 80;
+
+    // Speeds for wheels when intaking and launching. Intake speeds are negative to run the wheels
+    // in reverse
+    public static final double kLauncherSpeed = 1;
+    public static final double kLaunchFeederSpeed = 1;
+    public static final double kIntakeLauncherSpeed = -1;
+    public static final double kIntakeFeederSpeed = -.2;
+
+    public static final double kLauncherDelay = 1;
+  }
+
+ 
+public static class intakeConstants{
+
+
+  public static final double intakeSpeed = -1;
+  public static final int kFeederID = 5;
+
+}
+
+  
+
+  
+
+
 }
